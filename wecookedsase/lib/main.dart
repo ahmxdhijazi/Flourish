ce0acab29aaf52b61b346fad778d2db1a8dd1d53
Beau Fontenot <104475813+bfontenot23@users.noreply.github.com>
2025-11-08 22:04:59 -0600

    save captured photos to temporary location

--- wecookedsase/lib/main.dart
+++ wecookedsase/lib/main.dart
@@ -99,17 +99,30 @@ class _MainNavigationState extends State<MainNavigation> {
     ProfileScreen(),
   ];
 
+  void _onCameraButtonPressed(BuildContext context) {
+    final user = FirebaseAuth.instance.currentUser;
+    
+    if (user == null) {
+      // User is not signed in, show login screen
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const LoginScreen()),
+      );
+    } else {
+      // User is signed in, open camera
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const CameraScreen()),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: _screens[_currentIndex],
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => const CameraScreen()),
-          );
-        },
+        onPressed: () => _onCameraButtonPressed(context),
         backgroundColor: Colors.deepPurple,
         elevation: 6,
         child: Icon(
